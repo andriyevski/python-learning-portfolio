35cb0d108f879b54232a74f6ba55fb1abdc78140
Serhii Andriievskyi <andriyevski@gmail.com>
2025-07-14 00:32:59 +0300

    Fix+Add: Use,Change,Delete,Add in list...

--- crash_course/03_lists_intro/moto.py
+++ crash_course/03_lists_intro/moto.py
@@ -1,6 +1,7 @@
-# Use list and update him!
+# Use list
 motorcycles = ['honda','yamaha','suzuki']
 print(motorcycles)
 
+# Change 'honda' to 'ducati' in list motorcycles in index - 0
 motorcycles[0] = 'ducati'
 print(motorcycles)
