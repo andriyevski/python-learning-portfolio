7736368dd3a631c66eb1e9873b4d85c2c905088c
Serhii Andriievskyi <andriyevski@gmail.com>
2025-07-14 14:54:26 +0300

    03_lists_intro: Use and work with pop(),del,remove

--- crash_course/03_lists_intro/moto.py
+++ crash_course/03_lists_intro/moto.py
@@ -8,7 +8,7 @@ motorcycles[0] = 'ducati'
 print(motorcycles)
 #Shell#:  ['ducati','yamaha','suzuki']
 
-# Dellet all value in list
+# Delete all value in list
 motorcycles = []
 print(motorcycles)
 #Shell#: []
@@ -28,4 +28,48 @@ motorcycles.insert(0,'ducati')
 print(motorcycles)
 #Shell#:     ['ducati', 'honda', 'yamaha', 'suzuki']
 
-# Dell elements in list
+# Delete elements in list by index
+# Del -> index [] in list
+motorcycles = ['Honda','Yamaha','Suzuki']
+del motorcycles[2]
+print(motorcycles)
+#Shell#: ['Honda', 'Yamaha']
+
+# Delete element with use pop()
+motorcycles = ['Honda','Yamaha','Suzuki']
+motorcycles.pop()
+print(motorcycles)
+#Shell#: ['Honda', 'Yamaha']
+
+# Delete last element in list and greate new
+motorcycles = ['Honda','Yamaha','Suzuki']
+poped_motorcycles = motorcycles.pop()
+# poped_motorcycles is str
+print(poped_motorcycles)
+#Shell#: Suzuki
+
+# Last buy motorcycles
+motorcycles = ['Honda','Yamaha','Suzuki']
+last_owned = motorcycles.pop()
+print(f"The last motorcycles i owned was {last_owned.title()}")
+#Shell#: Suzuki
+
+# Pick from any one index in motorcycles
+motorcycles = ['Honda','Yamaha','Suzuki']
+first_owned = motorcycles.pop(0)
+print(f"Pick first index in motorcycles is {first_owned}")
+#Shell#: Pick first index in motorcycles is Honda
+
+# Delete elements by value
+motorcycles = ['Honda', 'Yamaha','Suzuki']
+motorcycles.remove('Yamaha')
+print(motorcycles)
+#Shell#: ['Honda', 'Suzuki']
+
+# Delete element and show him
+motorcycles = ['Honda', 'Yamaha','Suzuki']
+too_expensive  = 'Suzuki'
+motorcycles.remove(too_expensive)
+print(f"Thi is {too_expensive} too expensive for me!")
+#Shell#: This is Suzuki too expensive for me!
+# Remove delete only one time one first word ! Use while if you want delete all!
