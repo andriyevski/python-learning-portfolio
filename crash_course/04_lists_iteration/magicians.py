05f9486604af80cd206aacc0753414bd6baf216b
Serhii Andriievskyi <andriyevski@gmail.com>
2025-07-18 23:49:53 +0300

    .\magicians.py

--- crash_course/04_lists_iteration/magicians.py
+++ crash_course/04_lists_iteration/magicians.py
@@ -1,5 +1,13 @@
-##
+## Work with for in list
 #
 magicians = ['alice','david','carolina']
 for magician in magicians:
     print(magician)
+
+
+##If you forgot to indent
+#
+magicians = ['alice','david','carolina']
+for magician in magicians:
+    print(magician.title()+", That was a great trick!")
+print(f"I can't wait to see your next trick, {magician.title()}.\n")
