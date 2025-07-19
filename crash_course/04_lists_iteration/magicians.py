d1298f558f92abe4ef780135044c8364208a3b87
Serhii Andriievskyi <andriyevski@gmail.com>
2025-07-19 22:50:17 +0300

    Practice with lists and for-loops: iterating over words, fixing logic errors caused by incorrect indentation.

--- crash_course/04_lists_iteration/magicians.py
+++ crash_course/04_lists_iteration/magicians.py
@@ -2,12 +2,21 @@
 #
 magicians = ['alice','david','carolina']
 for magician in magicians:
-    print(magician)
+    print(magician+".\n")
 
 
-##If you forgot to indent
+##If you forgot to indent, print one times only
 #
 magicians = ['alice','david','carolina']
 for magician in magicians:
     print(magician.title()+", That was a great trick!")
 print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+## If we have a one more tab
+#
+magicians = ['Aliace','David','Carolina']
+for magician in magicians:
+    print(f'{magician.title()}, that was a great trick!')
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+    # Logical error - we take last name in ovetabundance of list
+    print(f"Thank you everyone, that was a great magic SHOW!{magician.title()}")
