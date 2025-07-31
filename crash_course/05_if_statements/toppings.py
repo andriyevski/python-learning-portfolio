c1c1d038ff852fa816d85d32b1b314b21c4a5e4f
Serhii Andriievskyi <andriyevski@gmail.com>
2025-08-01 01:02:41 +0300

    Modify: toppings.p > Add: more code for  explain and work!

--- crash_course/05_if_statements/toppings.py
+++ crash_course/05_if_statements/toppings.py
@@ -1,5 +1,5 @@
 # if-else explain
-requested_toppings = ['mashrooms','extra cheese']
+requested_toppings = ['mashrooms','green peppers','extra cheese']
 
 if 'mashrooms' in requested_toppings:
     print(f"Adding mashrooms.")
@@ -10,3 +10,21 @@ if 'extra cheese' in requested_toppings:
 else:
     print("I dont have ingradient!")
 print(f"Finished making your pizza!")
+
+# or use this # OPTIMIZE: code
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print(f"Sorry, we are out of green right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+print(f"Finished making your pizza!")
+
+# New task
+
+requested_toppings = []
+if requested_toppings:
+    if requested_topping == 'green peppers':
+        print(f"Sorry, we are out of green right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+print(f"Are you sure you want a plain pizza?")
