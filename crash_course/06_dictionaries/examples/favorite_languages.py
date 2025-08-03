8a2a6980f1923b4e414084a7dc1a6d40a8de8a74
Serhii Andriievskyi <andriyevski@gmail.com>
2025-08-04 02:42:03 +0300

    Add: few scripts, dict solution try

--- crash_course/06_dictionaries/examples/favorite_languages.py
+++ crash_course/06_dictionaries/examples/favorite_languages.py
@@ -7,3 +7,20 @@ favorite_languages = {
 }
 
 print(favorite_languages['sarah'])
+
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+# work with keys()
+for name in favorite_languages.keys():
+    print(name.title())
+
+
+# Friends task
+friends = ['phil','sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, i see you love {language}!")
