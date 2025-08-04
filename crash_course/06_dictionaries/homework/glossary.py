8b2b300342aca9729a71626cc29a51bb873a4191
Serhii Andriievskyi <andriyevski@gmail.com>
2025-08-04 19:40:37 +0300

    Add: rivers.py homework with solution!

--- crash_course/06_dictionaries/homework/glossary.py
+++ crash_course/06_dictionaries/homework/glossary.py
@@ -8,14 +8,14 @@ def popular_lang() -> Dict[str, object]:
     Five or more language in dict
     """
     lang_explain = {
-    'Python':'Simple but powerful language',
-    'c':'Hard, fast, clear language, you can control memory!',
-    'C#':'Simple if compare with C but have much more tools and good OOP',
-    'php':'Powerful for sites and 90% all sites in web write on PHP',
-    'js':'Simple dinamic language, uses for front most of all cases, but you can write for backand',
-    'go':'So cool for microservices!',
-    'java':'Old but good for banking and almoust all in real life what we use!',
-    'c++':'Used in every hard task if you need speed and memory control!'}
+        'Python':'Simple but powerful language',
+        'c':'Hard, fast, clear language, you can control memory!',
+        'C#':'Simple if compare with C but have much more tools and good OOP',
+        'php':'Powerful for sites and 90% all sites in web write on PHP',
+        'js':'Simple dinamic language, uses for front most of all cases, but you can write for backand',
+        'go':'So cool for microservices!',
+        'java':'Old but good for banking and almoust all in real life what we use!',
+        'c++':'Used in every hard task if you need speed and memory control!'}
     return lang_explain
 
 
