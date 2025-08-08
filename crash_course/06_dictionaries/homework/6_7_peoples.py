d64a4a9a76df3c8bf1258c1c95e7818afdf1bb77
Serhii Andriievskyi <andriyevski@gmail.com>
2025-08-08 23:52:03 +0300

    Add:6_7_peoples.py homework solution, not all but half!

--- crash_course/06_dictionaries/homework/6_7_peoples.py
+++ crash_course/06_dictionaries/homework/6_7_peoples.py
@@ -1,3 +1,28 @@
 # Homework from file tasks/peoples.md
 ## Task ## : 6.7
 # Peoples
+from typing import Dict
+
+
+Peoples_Dict = Dict[str,str]
+
+
+def get_peoples() -> Peoples_Dict:
+    """List with all users in dict"""
+    return  {
+        'first_name':'George',
+        'last_name':'Granovsky',
+        'age':'38',
+        'city':'Chicago',
+    }
+
+def print_peoples(users: Peoples_Dict) -> None:
+    """Just print all users"""
+    print(f"First name: {users['first_name']}\n")
+    print(f"Last name: {users['last_name']}\n")
+    print(f"Age: {users['age']}\n")
+    print(f"City: {users['city']}\n")
+
+
+if __name__ == "__main__":
+    print_peoples(get_peoples())
