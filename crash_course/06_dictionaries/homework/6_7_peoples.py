659ebf2b6d9108bc2a517caf65b0969fd30ee680
Serhii Andriievskyi <andriyevski@gmail.com>
2025-08-09 00:42:39 +0300

    Add 6_7_peoples.py and solution product ready with DRY

--- crash_course/06_dictionaries/homework/6_7_peoples.py
+++ crash_course/06_dictionaries/homework/6_7_peoples.py
@@ -1,28 +1,42 @@
 # Homework from file tasks/peoples.md
 ## Task ## : 6.7
 # Peoples
-from typing import Dict
+from typing import Dict, Any
 
 
-Peoples_Dict = Dict[str,str]
+Peoples_Dict = Dict[str,Any]
+
+class Person:
+    def __init__(self,first_name: str,last_name: str,age: int,city: str ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.city = city
 
 
-def get_peoples() -> Peoples_Dict:
-    """List with all users in dict"""
-    return  {
-        'first_name':'George',
-        'last_name':'Granovsky',
-        'age':'38',
-        'city':'Chicago',
-    }
+    def get_peoples(self) -> Peoples_Dict:
+        """List with all users in dict"""
+        return  {
+            'first_name':self.first_name,
+            'last_name':self.last_name,
+            'age':self.age,
+            'city':self.city,
+        }
 
-def print_peoples(users: Peoples_Dict) -> None:
-    """Just print all users"""
-    print(f"First name: {users['first_name']}\n")
-    print(f"Last name: {users['last_name']}\n")
-    print(f"Age: {users['age']}\n")
-    print(f"City: {users['city']}\n")
+    def print_info(self) -> None:
+        print(f"First name: {self.first_name}\n")
+        print(f"Last name: {self.last_name}\n")
+        print(f"Age: {self.age}\n")
+        print(f"City: {self.city}\n")
 
 
 if __name__ == "__main__":
-    print_peoples(get_peoples())
+    person1 = Person('Georg','Granovsky',38, 'Chicago')
+    person2 = Person('Harry','Potter',15, 'London')
+    person3 = Person('Bilbo','Heggens',38, 'Liverpool')
+
+    persons = [person1, person2, person3]
+
+    for user in persons:
+        user.print_info()
+        print("-"*50)
