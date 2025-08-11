786d9d51f248a57e5b517eaa7fab533db5d1bb89
Serhii Andriievskyi <andriyevski@gmail.com>
2025-08-11 23:50:57 +0300

    Add: 6_10_favorite_numbers.py -> solution for homework!

---
+++ crash_course/06_dictionaries/homework/6_10_favorite_numbers.py
@@ -0,0 +1,15 @@
+# Homework from file tasks/favorite_numbers.md
+## Task ## : 6.10
+# 6_10_ Favorite Numbers
+names = ['mike','zlotan','helga','artur','regina']
+numbers = [[5,6,7],[11,12,13],[7,14,21],[44,88,256,666],[777,999,1024,888]]
+
+for i in range(len(names)):
+    print(f"\n{names[i]}'s favorite numbers are:")
+    for number in numbers[i]:
+        print(f" - {number}")
+
+print("-"*20)
+print("Or use like this, more Pythonic!")
+print("-"*30)
+# or use this
