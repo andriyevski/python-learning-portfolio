e214d91ec6e24ff92a951a95a2bf0e7f8e7af1b6
Serhii Andriievskyi <andriyevski@gmail.com>
2025-08-13 23:30:03 +0300

    Add: simple homework task 6_11_cities.py

---
+++ crash_course/06_dictionaries/homework/6_11_cities.py
@@ -0,0 +1,11 @@
+# Homework from file tasks/cities.md
+## Task ## : 6.11
+# 6_11_ cities
+cities = {
+    'Kyiev':['Ukraine','5 250 000 people live here','capital of ukraine!'],
+    'Chernivtsi':['Ukraine','250 000 people live here','was romania,austria,moldovian in past!'],
+    'New York':['USA','15 250 000 people live here','Have a time square, mowt popular zone in city!'],
+}
+
+for citie in range(len(cities)):
+    print(f"Talk about {citie}: {citie[citie]}\n - Interesting fact: {citie}")
