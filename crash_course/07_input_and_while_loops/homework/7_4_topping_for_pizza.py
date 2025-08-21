811bd9a690f4ac1b1a24e586ee38e84be6547a5e
Serhii Andriievskyi <andriyevski@gmail.com>
2025-08-21 23:00:56 +0300

    Add: few examples,solutions and tasks!

---
+++ crash_course/07_input_and_while_loops/homework/7_4_topping_for_pizza.py
@@ -0,0 +1,25 @@
+# # Homework from file tasks/7_4_topping_for_pizza.md
+# ## Task ## : 7.4
+# # 7_4_topping_for_pizza
+# promt = "\nHello, what is the toppings for pizza you want?"
+# promt_2 ="\nChery,olives,cheese or something else? or 'quit' to exit!\n"
+#
+# toppings = []
+# chat = True
+#
+# while chat == True:
+#     if toppings:
+#         print(f"\nRight now you have next toppings: {toppings}")
+#         print(promt)
+#         answer = input()
+#         if answer == 'quit':
+#             chat = False
+#         else:
+#             toppings.append(answer)
+#     else:
+#         print(promt+promt_2)
+#         answer = input()
+#         if answer == 'quit':
+#             chat = False
+#         else:
+#             toppings.append(answer)
