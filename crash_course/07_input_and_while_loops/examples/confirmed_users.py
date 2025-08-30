32bcaeab2974f28377985db7bcc8de0e43631eab
Serhii Andriievskyi <andriyevski@gmail.com>
2025-08-30 23:27:37 +0300

    Learn While in examples!

--- crash_course/07_input_and_while_loops/examples/confirmed_users.py
+++ crash_course/07_input_and_while_loops/examples/confirmed_users.py
@@ -1,4 +1,5 @@
 unconfirmed_users = ['alice', 'brian', 'candace'] confirmed_users = []
 while unconfirmed_users: ❸
-current_user = unconfirmed_users.pop() print(f"Verifying user: {current_user.title()}")
+current_user = unconfirmed_users.pop()
+print(f"Verifying user: {current_user.title()}")
 confirmed_users.append(current_user)
