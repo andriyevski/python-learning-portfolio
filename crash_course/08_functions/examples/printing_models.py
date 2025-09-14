06ea0cdd61caeda0247f90ba8fc120cba8a01b95
Andriievskyi <andriyevski@gmail.com>
2025-09-14 17:16:03 +0300

    Update: 'print model' - change all to function !

--- crash_course/08_functions/examples/printing_models.py
+++ crash_course/08_functions/examples/printing_models.py
@@ -1,11 +1,34 @@
-# This program simulates printing 3D models.
-unprinted_design = ['phone case', 'robot pendant', 'dodecahedron']
+# # This program simulates printing 3D models.
+# unprinted_design = ['phone case', 'robot pendant', 'dodecahedron']
+# completed_models = []
+#
+# while unprinted_design:
+#     current_design = unprinted_design.pop()
+#     print(f"Printing model: {current_design}")
+#     completed_models.append(current_design)
+# print(f"\nThe following models have been printed:")
+# for completed_model in completed_models:
+#     print(completed_model)
+
+def print_models(unprinted_design, completed_models):
+    """
+    Imitation print model , when list do not stay empty
+    """
+    while unprinted_design:
+        current_design = unprinted_design.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    """
+    Print information about all printed models
+    """
+    print(f"\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_design = ['phone case', 'robot pendant', 'dodecahedron','apple case']
 completed_models = []
 
-while unprinted_design:
-    current_design = unprinted_design.pop()
-    print(f"Printing model: {current_design}")
-    completed_models.append(current_design)
-print(f"\nThe following models have been printed:")
-for completed_model in completed_models:
-    print(completed_model)
+print_models(unprinted_design, completed_models)
+show_completed_models(completed_models)
