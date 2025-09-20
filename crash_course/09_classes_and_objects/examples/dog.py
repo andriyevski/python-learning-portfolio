638cb9dee3fb56807218ad3b707dc9217417bba3
--global <andriyevski@gmail.com>
2025-09-21 01:04:15 +0300

    Add: simple explain class work with methods

--- crash_course/09_classes_and_objects/examples/dog.py
+++ crash_course/09_classes_and_objects/examples/dog.py
@@ -21,3 +21,18 @@ class Dog():
         Dog roll over when we give a command.
         """
         print(f"{self.name} rolled over!")
+
+
+my_dog = Dog('willie', 6)
+
+
+print(f"My dog name is {my_dog.name}")
+print(f"His age is {my_dog.age} year")
+my_dog.sit()
+my_dog.roll_over()
+
+my_dog2 = Dog('Gordon', 5)
+
+
+print(f"My second dog name is {my_dog2.name}")
+my_dog2.sit()
