097ef349be61bf4c6c7f3a45c272020990b123e8
--global <andriyevski@gmail.com>
2025-10-12 00:12:25 +0300

    Update: work with book, examples

--- crash_course/10_files_and_errors/examples/file_reader.py
+++ crash_course/10_files_and_errors/examples/file_reader.py
@@ -1,3 +1,19 @@
 with open("pi_digits.txt") as file_objects:
     contents = file_objects.read()
     print(contents.rstrip())
+
+
+"""
+Or use this to absolute path
+
+file_path = '/home/ehmatthes/other_files/text_files/имя_файла.txt'
+with open(file_path) as file_object:
+"""
+
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.replace(" ",""))
