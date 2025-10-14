9e9aabe6142867f53427582cb59d0d29805b535c
--global <andriyevski@gmail.com>
2025-10-14 22:35:23 +0300

    Update: change script to more PEP like

--- crash_course/10_files_and_errors/homework/10_4_Guest_Book.py
+++ crash_course/10_files_and_errors/homework/10_4_Guest_Book.py
@@ -4,11 +4,13 @@
 filename = 'guest_book.txt'
 
 while True:
-    name = input("Enter your name: ")
+    name = input("Enter your name: ").strip()
 
-    if name == 'q' or name == 'Q' or name == 'exit' or name == 'Exit':
+    # if name == 'q' or name == 'Q' or name == 'exit' or name == 'Exit':
+    if name.lower() in ['q','exit']:
+        print("\nGood Bye!\n")
         break
-    else:
+    elif name:
         with open(filename, 'a', encoding = 'utf-8') as file_obj:
             file_obj.write(name+'\n')
             print(name+'\n')
