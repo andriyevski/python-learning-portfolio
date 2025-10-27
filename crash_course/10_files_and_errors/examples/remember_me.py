19233068784d2df33d3a80726ec94b42e98c11fb
--global <andriyevski@gmail.com>
2025-10-27 23:00:46 +0200

    Update: remember_me.py script and make more details and logic with exception!

--- crash_course/10_files_and_errors/examples/remember_me.py
+++ crash_course/10_files_and_errors/examples/remember_me.py
@@ -1,8 +1,21 @@
+# import json
+#
+# username = input("What is your name? ")
+#
+# filename = 'username.json'
+# with open(filename, 'w', encoding = 'utf-8') as f:
+#     json.dump(username, f)
+#     print(f"We'll remember you when you come back, {username}!")
 import json
 
-username = input("What is your name? ")
-
 filename = 'username.json'
-with open(filename, 'w', encoding = 'utf-8') as f:
-    json.dump(username, f)
-    print(f"We'll remember you when you come back, {username}!")
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        json.dump(username, f)
+        print(f"We'll remember you when you come back, {username}!")
+else:
+    print(f"Welcome back, {username}!")
