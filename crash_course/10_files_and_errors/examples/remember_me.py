2390ab1b617306800ea164fab8e435643371137d
--global <andriyevski@gmail.com>
2025-10-28 17:15:00 +0200

    Update: change script to more perfect in work!

--- crash_course/10_files_and_errors/examples/remember_me.py
+++ crash_course/10_files_and_errors/examples/remember_me.py
@@ -6,16 +6,68 @@
 # with open(filename, 'w', encoding = 'utf-8') as f:
 #     json.dump(username, f)
 #     print(f"We'll remember you when you come back, {username}!")
+
+
+# import json
+#
+# filename = 'username.json'
+# try:
+#     with open(filename) as f:
+#         username = json.load(f)
+# except FileNotFoundError:
+#     username = input("What is your name? ")
+#     with open(filename, 'w', encoding = 'utf-8') as f:
+#         json.dump(username, f)
+#         print(f"We'll remember you when you come back, {username}!")
+# else:
+#     print(f"Welcome back, {username}!")
+
+# import json
+#
+# def greet_user():
+#     """ Greet the user by name """
+#     filename = 'username.json'
+#     try:
+#         with open(filename) as f:
+#             username = json.load(f)
+#     except FileNotFoundError:
+#         username = input("What is your name? ")
+#         with open(filename, 'w', encoding = 'utf-8') as f:
+#             json.dump(username, f)
+#             print(f"We'll remember you when you come back, {username}!")
+#     else:
+#         print(f"Welcome back, {username}!")
+#
+# greet_user()
+
 import json
 
-filename = 'username.json'
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
+def get_stored_username():
+    """ Take saved name a user if they created """
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_user():
     username = input("What is your name? ")
-    with open(filename, 'w', encoding = 'utf-8') as f:
+    filename = 'username.json'
+    with open(filename, 'w') as f:
         json.dump(username, f)
-        print(f"We'll remember you when you come back, {username}!")
-else:
-    print(f"Welcome back, {username}!")
+    return username
+
+def greet_user():
+        username = get_stored_username()
+        if username:
+            print(f"Welcome back, {username}!")
+        else:
+            username = get_new_user()
+            print(f"We'll remember you when you come back, {username}!")
+
+
+
+greet_user()
