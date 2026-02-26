b5f5f8d34d455ba9956e49fef4fe87936a504044
--global <andriyevski@gmail.com>
2026-02-27 00:06:24 +0200

    Add: return body message

--- FastApi_course/hello.py
+++ FastApi_course/hello.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 @app.get("/hi/{who}")
 def greet(who):
     return f"Hello? {who}?"
+
+@app.post("/hi")
+def greet_post(who:str = Body(embed=True)):
+    return f"Hello? {who}"
 
 if __name__ == "__main__":
     import uvicorn
